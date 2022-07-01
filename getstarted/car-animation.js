7baf3597e6b1a56fd447f6bf21700774fa546e9f
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2022-07-01 13:42:20 +0700

    building a car

---
+++ getstarted/car-animation.js
@@ -0,0 +1,78 @@
+var canvas = document.getElementById('renderCanvas');
+
+var startRenderLoop = function (engine, canvas) {
+  engine.runRenderLoop(function () {
+    if (sceneToRender && sceneToRender.activeCamera) {
+      sceneToRender.render();
+    }
+  });
+};
+
+var engine = null;
+var scene = null;
+var sceneToRender = null;
+var createDefaultEngine = function () {
+  return new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+    disableWebGL2Support: false,
+  });
+};
+const createScene = () => {
+  const scene = new BABYLON.Scene(engine);
+
+  /**** Set camera and light *****/
+  const camera = new BABYLON.ArcRotateCamera(
+    'camera',
+    -Math.PI / 2,
+    Math.PI / 2.5,
+    2,
+    new BABYLON.Vector3(0, 0, 0)
+  );
+
+  camera.attachControl(canvas, true);
+  const light = new BABYLON.HemisphericLight(
+    'light',
+    new BABYLON.Vector3(1, 1, 0)
+  );
+
+  BABYLON.SceneLoader.ImportMeshAsync('', '', 'car.glb').then(() => {
+    const wheelRB = scene.getMeshByName('wheelRB');
+    const wheelRF = scene.getMeshByName('wheelRF');
+    const wheelLB = scene.getMeshByName('wheelLB');
+    const wheelLF = scene.getMeshByName('wheelLF');
+
+    scene.beginAnimation(wheelRB, 0, 30, true);
+    scene.beginAnimation(wheelRF, 0, 30, true);
+    scene.beginAnimation(wheelLB, 0, 30, true);
+    scene.beginAnimation(wheelLF, 0, 30, true);
+  });
+
+  return scene;
+};
+
+window.initFunction = async function () {
+  var asyncEngineCreation = async function () {
+    try {
+      return createDefaultEngine();
+    } catch (e) {
+      console.log(
+        'the available createEngine function failed. Creating the default engine instead'
+      );
+      return createDefaultEngine();
+    }
+  };
+
+  window.engine = await asyncEngineCreation();
+  if (!engine) throw 'engine shoguld not be null.';
+  startRenderLoop(engine, canvas);
+  window.scene = createScene();
+};
+initFunction().then(() => {
+  sceneToRender = scene;
+});
+
+// Resize
+window.addEventListener('resize', function () {
+  engine.resize();
+});
